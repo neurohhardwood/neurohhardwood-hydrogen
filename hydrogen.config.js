c134bd9fa9aba0ee9185577fe992dc46ff7b85f0
Cavell Blood <cavellblood@gmail.com>
2022-05-31 18:11:48 -0500

    Initial Commit

---
+++ hydrogen.config.js
@@ -0,0 +1,21 @@
+import {defineConfig} from '@shopify/hydrogen/config';
+import {
+  CookieSessionStorage,
+  PerformanceMetricsServerAnalyticsConnector,
+} from '@shopify/hydrogen';
+
+export default defineConfig({
+  shopify: {
+    storeDomain: 'neuroh-hardwood.myshopify.com',
+    storefrontToken: '5e4d5defd9fdf9130909bef5ffe64fe5',
+    storefrontApiVersion: '2022-07',
+  },
+  session: CookieSessionStorage('__session', {
+    path: '/',
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'strict',
+    maxAge: 60 * 60 * 24 * 30,
+  }),
+  serverAnalyticsConnectors: [PerformanceMetricsServerAnalyticsConnector],
+});
