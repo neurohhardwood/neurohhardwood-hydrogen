425883ff05a2954d8fcf61d081562cf5dd654760
Cavell Blood <cavellblood@gmail.com>
2022-08-08 14:36:48 -0500

    Revert to using `sed` to replace placeholder token

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,6 +3,6 @@ export default {
   dataset: import.meta.env.VITE_SANITY_STUDIO_API_DATASET || 'production',
   projectId: 'f3wu28ux',
   token:
-    'skb8Qwi6HICEx9tGVHRCwRlbQeLR2ooSwY3daR9sFDZdzOJLVxBhAUUSIL2yZgHheAygQjy3m7eic33G0xO2lk08Tb2KLLRu7yAzmLIQM61duMPLP980H09tPcNldsiMIOBnhKrhF7g2jPKrK6JB0yNKYmGPmaU5ouEpJ0CSB8WnE71O0OJt',
+    import.meta.env.VITE_SANITY_NETLIFY_TOKEN || 'SANITY_TOKEN_PLACEHOLDER',
   useCdn: true,
 };
