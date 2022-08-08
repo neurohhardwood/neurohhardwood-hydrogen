eed44ed9dd5e05e595120ae7a387cf13df86fcf4
Cavell Blood <cavellblood@gmail.com>
2022-08-08 12:58:51 -0500

    Try updating token here

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,6 @@ export default {
   apiVersion: 'v2022-07-01',
   dataset: import.meta.env.VITE_SANITY_STUDIO_API_DATASET || 'production',
   projectId: 'f3wu28ux',
-  token: import.meta.env.VITE_SANITY_NETLIFY_TOKEN || process.env.SANITY_TOKEN,
+  token: import.meta.env.VITE_SANITY_NETLIFY_TOKEN || $SANITY_TOKEN,
   useCdn: true,
 };
