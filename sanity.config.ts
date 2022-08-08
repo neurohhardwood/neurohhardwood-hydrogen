03da9dba16171fd3b0b27e872ebd1621e3cbe353
Cavell Blood <cavellblood@gmail.com>
2022-08-08 13:26:17 -0500

    Add token

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,5 +2,7 @@ export default {
   apiVersion: 'v2022-07-01',
   dataset: import.meta.env.VITE_SANITY_STUDIO_API_DATASET || 'production',
   projectId: 'f3wu28ux',
+  token:
+    import.meta.env.VITE_SANITY_NETLIFY_TOKEN || 'SANITY_TOKEN_PLACEHOLDER',
   useCdn: true,
 };
