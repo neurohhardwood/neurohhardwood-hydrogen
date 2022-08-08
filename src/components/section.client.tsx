1de0d54dea8eaa97387d5c29a98f0ef8bc7818e2
Cavell Blood <cavellblood@gmail.com>
2022-08-07 20:58:53 -0500

    Panels are looking better

---
+++ src/components/section.client.tsx
@@ -0,0 +1,59 @@
+import {Disclosure} from '@headlessui/react';
+import clsx from 'clsx';
+
+export const Section = ({panel, index}) => {
+  return (
+    <section
+      className="relative  z-[1]  flex  min-h-[90vh]  flex-col  justify-center  overflow-hidden  bg-sambuca-500  bg-cover  bg-center  bg-no-repeat"
+      style={{
+        backgroundImage:
+          "url('https://s3.us-east-2.amazonaws.com/assets.neurohhardwood.com/photos/_deskBkg/78D52D1E-6A30-4777-8DD0-9209300FCBBE.JPG')",
+        backgroundPosition: '50% 50%',
+      }}
+    >
+      <div
+        className="absolute  inset-0"
+        style={{clip: 'rect(auto auto auto auto)'}}
+      >
+        <div
+          className={clsx(
+            'pointer-events-none  top-0  min-h-[100vh]  w-full  overflow-hidden  bg-cover  bg-center  bg-no-repeat',
+            index % 2 == 0
+              ? 'c-section__background--scroll'
+              : 'c-section__background--fixed',
+          )}
+          style={{
+            backgroundImage:
+              "url('https://s3.us-east-2.amazonaws.com/assets.neurohhardwood.com/photos/_deskBkg/78D52D1E-6A30-4777-8DD0-9209300FCBBE.JPG')",
+            backgroundPosition: '50% 50%',
+            backfaceVisibility: 'hidden',
+            transform: index % 2 == 0 ? 'translateZ(0)' : '',
+            zIndex: -index,
+          }}
+        ></div>
+        <div
+          className={clsx(
+            index % 2 == 0 ? 'absolute  inset-0' : 'fixed  inset-0',
+            'px-6',
+          )}
+          style={{transform: 'translateZ(0)'}}
+        >
+          <Disclosure
+            as="div"
+            className="flex  h-full  flex-col  items-center  justify-center"
+          >
+            <Disclosure.Button className="py-2">
+              <h1 className="section-title  text-[5rem]  font-light  text-white">
+                {panel.title}
+              </h1>
+            </Disclosure.Button>
+            <Disclosure.Panel className="text-gray-500">
+              Yes! You can purchase a license that you can share with your
+              entire team.
+            </Disclosure.Panel>
+          </Disclosure>
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/routes/index.server.tsx
+++ src/routes/index.server.tsx
@@ -1,6 +1,7 @@
 import {type HydrogenRouteProps, Image, Seo} from '@shopify/hydrogen';
 import clsx from 'clsx';
 import groq from 'groq';
+import {Section} from '~/components/section.client';
 import useSanityQuery from '~/hooks/useSanityQuery';
 
 export default function Homepage({params}: {params?: HydrogenRouteProps}) {
@@ -19,38 +20,7 @@ export default function Homepage({params}: {params?: HydrogenRouteProps}) {
     <div>
       {entry.panels.map((panel, indexZero) => {
         const index = indexZero + 1;
-        const backgroundBehavior = index % 2 == 0 ? 'scroll' : 'fixed';
-
-        return (
-          <section
-            className="translate-  relative  z-[1]  flex  min-h-[90vh]  flex-col  justify-center  overflow-hidden  bg-sambuca-500  bg-cover  bg-center  bg-no-repeat"
-            style={{
-              backgroundImage:
-                "url('https://s3.us-east-2.amazonaws.com/assets.neurohhardwood.com/photos/_deskBkg/78D52D1E-6A30-4777-8DD0-9209300FCBBE.JPG')",
-              backgroundPosition: '50% 50%',
-            }}
-            key={panel._id}
-          >
-            <div className="" data-background-type={backgroundBehavior}>
-              <div
-                className={clsx(
-                  'pointer-events-none  top-0  min-h-[100vh]  w-full  overflow-hidden  bg-cover  bg-center  bg-no-repeat',
-                  index % 2 == 0 ? 'relative' : 'fixed',
-                )}
-                style={{
-                  backgroundImage:
-                    "url('https://s3.us-east-2.amazonaws.com/assets.neurohhardwood.com/photos/_deskBkg/78D52D1E-6A30-4777-8DD0-9209300FCBBE.JPG')",
-                  backgroundPosition: '50% 50%',
-                  backfaceVisibility: 'hidden',
-                  transform: index % 2 == 0 ? 'translateZ(0px)' : '',
-                  zIndex: -index,
-                }}
-              ></div>
-
-              <h1>{panel.title}</h1>
-            </div>
-          </section>
-        );
+        return <Section panel={panel} index={index} key={panel._id} />;
       })}
     </div>
   );
