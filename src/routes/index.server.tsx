c5ec23b0f72bcd0de59ccfe671d9977094efff6a
Cavell Blood <cavellblood@gmail.com>
2022-08-22 10:37:40 -0500

    Add conditional for if entry is not defined.

--- src/routes/index.server.tsx
+++ src/routes/index.server.tsx
@@ -12,10 +12,10 @@ export default function Homepage({params}: {params?: HydrogenRouteProps}) {
     query: QUERY_SANITY_HOMEPAGE,
   });
 
-  // if (!entry) {
-  //   // @ts-expect-error <NotFound> doesn't require response
-  //   return <h1>Not Found</h1>;
-  // }
+  if (!entry) {
+    // @ts-expect-error <NotFound> doesn't require response
+    return <h1>Not Found</h1>;
+  }
 
   return (
     <div>
