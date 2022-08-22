ea6a6f06b82165f71431e6684b65ca4e128bdf43
Cavell Blood <cavellblood@gmail.com>
2022-08-22 09:43:06 -0500

    Restore token

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,5 +2,6 @@ export default {
   apiVersion: 'v2022-07-01',
   dataset: 'production',
   projectId: 'f3wu28ux',
+  token: import.meta.env.VITE_SANITY_NETLIFY_TOKEN || process.env.SANITY_TOKEN,
   useCdn: true,
 };
