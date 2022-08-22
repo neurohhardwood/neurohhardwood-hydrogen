39512fb13fcd722ac9c8bc412f3498dff2fda5b1
Cavell Blood <cavellblood@gmail.com>
2022-08-22 12:37:32 -0500

    Remove log

--- src/routes/index.server.tsx
+++ src/routes/index.server.tsx
@@ -12,8 +12,6 @@ export default function Homepage({params}: {params?: HydrogenRouteProps}) {
     query: QUERY_SANITY_HOMEPAGE,
   });
 
-  console.log(entry);
-
   if (!entry) {
     // @ts-expect-error <NotFound> doesn't require response
     return <h1>Not Found</h1>;
