330be2028b7c8c6750d117ed76a147701309ac56
Cavell Blood <cavellblood@gmail.com>
2022-08-31 18:04:15 -0500

    Fix background image issue on Safari

--- src/components/section.client.tsx
+++ src/components/section.client.tsx
@@ -17,17 +17,18 @@ export const Section = ({panel, index}) => {
       >
         <div
           className={clsx(
+            'c-section__background--image',
             'pointer-events-none  top-0  min-h-[100vh]  w-full  overflow-hidden  bg-cover  bg-center  bg-no-repeat',
             index % 2 == 0
               ? 'c-section__background--scroll'
-              : 'c-section__background--fixed',
+              : 'c-section__background--fixed  fixed',
           )}
           style={{
             backgroundImage:
               "url('https://s3.us-east-2.amazonaws.com/assets.neurohhardwood.com/photos/_deskBkg/78D52D1E-6A30-4777-8DD0-9209300FCBBE.JPG')",
             backgroundPosition: '50% 50%',
             backfaceVisibility: 'hidden',
-            transform: index % 2 == 0 ? 'translateZ(0)' : '',
+            transform: index % 2 == 0 ? '' : 'translateZ(0)',
             zIndex: -index,
           }}
         ></div>
